da063d0d3bd71c751860d6068c0d2fa681c913aa
Anthony Tranduc <anthonytranduc@gmail.com>
2019-09-23 22:22:28 -0400

    finished layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './styles/styles.css';
 
-const app = () => <div className="app"></div>;
+import Calculator from './layout/Calculator/Calculator';
+
+const app = () => (
+  <div className="app">
+    <Calculator />
+  </div>
+);
 
 export default app;
